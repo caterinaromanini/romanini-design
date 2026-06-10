00561e55954fffe7661d8872b430b030ca6ddd54
caterinaromanini <caterinaromaninil@gmail.com>
2026-06-09 20:51:02 -0500

    Update Hero.js

--- romanini-design/components/Hero.js
+++ romanini-design/components/Hero.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { scrollTo } from "@/lib/utils";
+import { scrollTo } from "../lib/utils";
 
 export default function Hero() {
   const [loaded, setLoaded] = useState(false);
